434a5294d208c9a7e253ca46881d0a43cdafa59c
Your Namede <edoardo.monti.bo@gmail.com>
2023-04-27 17:39:24 +0200

    aggiustaggi

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@ Milestone 1
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 
+Milestone 2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione
+● Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+“mar” rimangono solo Marco e Martina)
 */
 
 
@@ -186,7 +201,8 @@ createApp({
         addElem(){
             let userText={
                 message: this.textChat,
-                status: 'sent'
+                status: 'sent',
+                date: '10/01/2020 15:52:00'
             }
             
 
@@ -201,7 +217,8 @@ createApp({
             setTimeout(() =>{
                 let responce={
                     message: this.textReceived,
-                    status: 'received'
+                    status: 'received',
+                    date: '10/01/2020 15:53:00'
                 }
                 this.contacts[this.active].messages.push(responce)
             }, 1000)
